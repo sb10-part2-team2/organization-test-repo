dda49e4096503d8be9563388f9e6d591da7a219d
raonPsm <s102075@naver.com>
2026-03-08 11:10:52 +0900

    docs: README.md 작성 예시

---
+++ src/main/java/com/example/organizationtestrepo/OrganizationTestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.organizationtestrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationTestRepoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OrganizationTestRepoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/organizationtestrepo/OrganizationTestRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.organizationtestrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationTestRepoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
